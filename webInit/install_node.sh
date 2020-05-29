24fbdc23bf66c794ae73ddaa5a72df34461a8d69
子勰 <code@bihe0832.com>
2020-05-30 01:18:11 +0800

    修正

--- webInit/install_node.sh
+++ webInit/install_node.sh
@@ -18,6 +18,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
 nvm --version
+nvm install node
 cd ~/.nvm 
 git fetch -p
 git checkout $(git describe --tags `git rev-list --tags --max-count=1`)
