782e191e64dee00b30e38a4aab6e9fc7df006136
zixie <code@bihe0832.com>
2021-01-26 15:04:38 +0800

    mac init

--- tools/mac_init.sh
+++ tools/mac_init.sh
@@ -16,21 +16,38 @@ ln -s /Volumes/Document/Documents ~/Documents
 ln -s ~/Documents ~/zixie
 ln -s /Users/zixie/lib ~/zixie/lib
 cd /Volumes/Document/Documents
+mkdir web
 mkdir temp
 ln -s /Volumes/Document/Documents/temp ~/temp
 cd ~/temp 
 mkdir 1
-mkdir web
+
+# 环境配置
+cd  ~/zixie/
+mkdir github
+cd ~/zixie/github
+git clone https://github.com/bihe0832/bihe0832.github.io.git blog
+git clone https://github.com/bihe0832/Settings-Tools.git
+git clone https://github.com/bihe0832/gitbook.git
+cd  ~/zixie/github/gitbook
+echo "favicon
+donate" | xargs -I {} npm install gitbook-plugin-{}
+npm install
+
+cp -fr ~/zixie/github/Settings-Tools/config/.oh-my-zsh ~/.oh-my-zsh
+cp -fr ~/zixie/github/Settings-Tools/config/mac/.zshrc ~/.zshrc
+source ~/.zshrc
 
 # 基础库安装
 brew reinstall autoconf gifsicle readline xz dos2unix thefuck tree wget sqlite zsh-syntax-highlighting autojump bat git-open zsh-autosuggestions
-
+brew update
 # python
 brew reinstall python@2 python3
 
 # jekyll
 sudo gem uninstall --all
-brew install ruby@2.6
+brew reinstall ruby@2.6
+source ~/.zshrc
 gem install ffi -- --disable-system-libffi 
 gem install jekyll
 gem install github-pages
@@ -57,22 +74,10 @@ nrm test
 nrm use taobao
 npm install gitbook-cli -g
 
-# 环境配置
-cd  ~/zixie/
-mkdir github
-cd ~/zixie/github
-git clone https://github.com/bihe0832/bihe0832.github.io.git blog
-git clone https://github.com/bihe0832/Settings-Tools.git
-git clone https://github.com/bihe0832/gitbook.git
-cd  ~/zixie/github/gitbook
-echo "favicon
-donate" | xargs -I {} npm install gitbook-plugin-{}
-npm install
-
-cp -fr ~/zixie/github/Settings-Tools/config/.oh-my-zsh ~/.oh-my-zsh
-cp -fr ~/zixie/github/Settings-Tools/config/mac/.zshrc ~/.zshrc
-source ~/.zshrc
 
 # apache
 sudo chmod -R 755 /Volumes/Document/Documents/web/
 sudo cp -fr ~/zixie/github/Settings-Tools/config/httpd-vhosts.conf /etc/apache2/extra/httpd-vhosts.conf
+
+brew cleanup
+brew doctor
